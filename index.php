434e6e0e02a137733e73410eb662efa850764fd0
kalabtec <91256245+kalabtec@users.noreply.github.com>
2022-06-05 08:42:21 -0300

    Criação index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/style.css">
+  <title> Login</title>
+</head>
+
+<body>
+ <section class="area-login">
+    <div class="login">
+        <div>
+          <img src="">
+        </div>
+        
+        <form method="POST">
+          <input type="text" name="nome" placeholder="nome de usuario" autofocus>
+          <input type="password" name="senha" placeholder="sua senha">
+          <input type="submit" value="Entrar">
+        </form>
+    </div>
+  </section>
+</body>
+</html>
+  
